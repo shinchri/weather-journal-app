1bd4d886aa6b66df138b6e8a9795a59ab4664fb0
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-09-16 19:43:56 -0500

    feat: Create method to retrieve date, and declare constant variables for urls and key

--- website/app.js
+++ website/app.js
@@ -1,5 +1,12 @@
 /* Global Variables */
+const backendURL = "http://localhost:8000";
+const weatherKey = "0bdae6985d07a46821ad131da0c78aca";
+const weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=" + weatherKey;
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+function getRecentDate() {
+    let d = new Date();
+    let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+    return newDate;
+}
+
